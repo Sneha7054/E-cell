2d95ddc91ecac6f8a085c5ac80fb2c6080a213fd
Sneha7054 <sneha754513@gmail.com>
2025-01-23 14:22:16 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import FirstPage from './pages/FirstPage';
+import SecondPage from './pages/SecondPage';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<FirstPage />} />
+        <Route path="/second" element={<SecondPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
